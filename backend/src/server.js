d5e84b3344078fb294203c35db2098be733c607e
alifarhan1531 <alifarhan1531@gmail.com>
2025-07-17 22:19:42 +0500

    minor chnage

--- backend/src/server.js
+++ backend/src/server.js
@@ -72,7 +72,7 @@ const app = express();
 const server = http.createServer(app); 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://codezynx.netlify.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
